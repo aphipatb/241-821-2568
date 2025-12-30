57e2a04700bcce680722fc6ac355449c1710966a
aphipatb <aphipat.b@ku.th>
2025-12-30 11:07:58 +0700

    java

---
+++ JS/main.js
@@ -0,0 +1,107 @@
+//console.log("hello word")
+//console.log("this is a test JavaScript")
+
+//String, Number, Boolen,Object,Array 
+
+
+/**
+let fname ="john";
+console.log("Name:",fname);
+const PI=3.14;
+
+let age =30;
+
+let height= 5.9;
+
+
+fname = "Alice";
+fname = "Bob";
+console.log("Name:",fname);
+console.log("Age:",age);
+console.log("Height",height);
+*/
+
+
+/**
+ let number1="10";
+let number2="3";
+
+let result1 =number1+ number2;
+console.log("ผลบวก:",result1);
+
+ */
+
+
+/** 
+let number1 =10;
+let number2=20;
+let condition = number1 < number2; //boolen ค่าความจริง  true หรือfalse
+onsole.log("condition",condition);
+*/
+
+
+
+/**
+//if-else condition
+let number1 =3;
+let number2=5;
+if(number1 > number2){
+    console.log("this is if");
+
+}
+else if(number1 < number2){
+    console.log("this is else if");
+}
+else{
+    console.log("this is else");
+}
+ */
+
+// let scores =prompt("ใส่คะแนนของคุณ:");
+// if(scores>=80){
+//     console.log("Grade = A");
+// }
+// else if (scores>=70){
+//     console.log("Grade = B");
+// }
+// else if (scores>=60){
+//     console.log("Grade = C");
+// }
+// else if (scores>=50){
+//     console.log("Grade = D");
+// }
+// else{
+//     console.log("Grade = F");
+// }
+
+/** 
+let number1 = 5;
+let number2 =10;
+
+let condition =(number1>0) || (number2>0);
+console.log("condition", condition);
+
+
+let age =25;
+let gender = "female"
+if(age >=18 && gender=="female"){
+    console.log("คุณสามารถเข้าร่วมกิจกรรมได้");
+} 
+    */
+/**    
+let number1 =20;
+if (number1 % 2 == 0){
+    console.log("เป็นเลขคู่");
+}else{
+    console.log("เป็นเลขคี่")
+}
+*/
+
+let conter =0;
+while(conter <= 9){
+    conter =conter+1;
+    console.log("while",conter);
+}
+for(let i=0;i<=4;i=i+1){
+    console.log("for",i)
+}
